b686a613aeb4ac241d368a736133ff054a1de174
Adarsh Bhat <kanakbhat6@gmail.com>
2020-05-04 23:16:01 +0530

    Comment : D1.4 on Mon May  4 23:16:01 IST 2020

---
+++ configs/netinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt install xserver-xorg xinit git curl wget
+sudo apt install slim
+# sudo apt install jwm
+sudo apt install icewm
+# sudo apt install i3
+sudo apt install pcmanfm --no-install-recommends
+sudo apt install libfm-modules gvfs gvfs-backends gnome-themes-standard lxinput lxrandr mlocate mousepad sakura gtk2-engines arandr
+sudo apt install gtk2-engines-murrine --no-install-recommends
+sudo apt install ntp ntpdate lxappearance ufw bleachbit gdebi synaptic dmz-curor-theme volumeicon-alsa compton conky alsa-utils pavucontrol ttf-dejavu yad psmisc acpid locales-all xserver-xorg-input-synaptics cmst libgtk3-perl
+sudo apt install file-roller rofi dmenu alacarte dkms 
